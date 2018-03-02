202c379c48d5b8d846d25bacf4bce96ccc3bfc47
Nukleon84 <jochen.steimel@googlemail.com>
2018-03-02 19:59:01 +0100

    Removed commented code

--- source.contracts/OpenFMSL.Contracts/OpenFMSL.Core/Thermodynamics/FlashRoutines.cs
+++ source.contracts/OpenFMSL.Contracts/OpenFMSL.Core/Thermodynamics/FlashRoutines.cs
@@ -42,9 +42,7 @@ namespace OpenFMSL.Core.Thermodynamics
             PrecalculateTP(copy);
 
             var problem2 = new EquationSystem() { Name = "PQ-Flash" };
-
-            //  var tempStream = new MaterialStream("temp", stream.System);
-            // tempStream.CopyFrom(stream);
+          
             copy.GetVariable("p").IsFixed = true;
             copy.GetVariable("T").IsFixed = false;
             copy.GetVariable("VF").IsFixed = false;
@@ -64,11 +62,6 @@ namespace OpenFMSL.Core.Thermodynamics
             performEnthalpyUpdate(copy);
 
             stream.CopyFrom(copy);
-           // performMassBalance(stream, copy.KValues);
-           // performDensityUpdate(stream);
-           // performEnthalpyUpdate(stream);
-
-
             return true;
 
         }
@@ -124,44 +117,6 @@ namespace OpenFMSL.Core.Thermodynamics
                 stream.Liquid.ComponentMolarFraction[i].ValueInSI = stream.Mixed.ComponentMolarFraction[i].ValueInSI;
                 stream.Vapor.ComponentMolarFraction[i].ValueInSI = stream.Mixed.ComponentMolarFraction[i].ValueInSI;
             }
-
-            //Calculate Dew Point and Bubble Point
-
-            /*  var KD = new Variable[stream.System.Components.Count];
-              var KB = new Variable[stream.System.Components.Count];
-              var TD = stream.System.VariableFactory.CreateVariable("TD", "Dew-point temperature", PhysicalDimension.Temperature);
-              var TB = stream.System.VariableFactory.CreateVariable("TB", "Bubble-point temperature", PhysicalDimension.Temperature);
-
-              for (int i = 0; i < NC; i++)
-              {
-                  KD[i] = stream.System.VariableFactory.CreateVariable("KD", "Equilibrium distribution coefficient", PhysicalDimension.Dimensionless);
-                  KD[i].Subscript = stream.System.Components[i].ID;
-                  KD[i].Group = "Dew-Point";
-                  KD[i].ValueInSI = 1.4;
-
-                  KB[i] = stream.System.VariableFactory.CreateVariable("KB", "Equilibrium distribution coefficient", PhysicalDimension.Dimensionless);
-                  KB[i].Subscript = stream.System.Components[i].ID;
-                  KB[i].Group = "Bubble-Point";
-                  KB[i].ValueInSI = 1.4;
-              }
-              var problem = new EquationSystem() { Name = "Phase boundary" };
-              for (int i = 0; i < NC; i++)
-              {
-                  stream.System.EquationFactory.EquilibriumCoefficient(stream.System, KD[i], TD, stream.Mixed.Pressure, stream.Liquid.ComponentMolarFraction, stream.Vapor.ComponentMolarFraction, i);
-                  stream.System.EquationFactory.EquilibriumCoefficient(stream.System, KB[i], TB, stream.Mixed.Pressure, stream.Liquid.ComponentMolarFraction, stream.Vapor.ComponentMolarFraction, i);
-                  stream.System.EquationFactory.EquilibriumCoefficient(stream.System, stream.KValues[i], stream.Mixed.Temperature, stream.Mixed.Pressure, stream.Liquid.ComponentMolarFraction, stream.Vapor.ComponentMolarFraction, i);
-
-              }
-              problem.AddConstraints((Sym.Sum(0, NC, i => stream.Mixed.ComponentMolarFraction[i] * KB[i])).IsEqualTo(1));
-              problem.AddConstraints((Sym.Sum(0, NC, i => stream.Mixed.ComponentMolarFraction[i] / KD[i])).IsEqualTo(1));
-
-              problem.AddDefinedVariables(KD);
-              problem.AddDefinedVariables(KB);
-
-              problem.AddVariables(TD);
-              problem.AddVariables(TB);
-              _solver.Solve(problem);*/
-
 
             //Solving Rachford Rice equation to get actual vapour fraction
 
@@ -300,164 +255,7 @@ namespace OpenFMSL.Core.Thermodynamics
         }
 
 
-        #region Flash Calculation
-        /*
-          override bool Initialize()
-          {
-              int NC = ComponentSet.Count;
-
-              //T,P Flash
-              if (Mixed.Temperature.IsFixed && Mixed.Pressure.IsFixed)
-              {
-                  performTPFlash();
-              }
-              //x,P Flash
-              else if (_vf.IsFixed && Mixed.Pressure.IsFixed)
-              {
-                  performXPFlash();
-              }
-              else
-              {
-                  performTPFlash();
-              }
-
-              performMassBalance();
-
-
-              performDensityUpdate();
-
-
-              performEnthalpyUpdate(NC);
-
-
-              return true;
-          }
-
-          private void performXPFlash()
-          {
-              int NC = System.Components.Count;
-
-              //if (_vf.Value == 0)
-              //{
-              //    State = PhaseState.BubblePoint;
-              //}
-              //else if (_vf.Value == 1)
-              //{
-              //    State = PhaseState.DewPoint;
-              //}
-              //else
-              //{
-              //    State = PhaseState.LiquidVapour;
-              //}
-
-              var problem = new NLP();
-              problem.Name = "Flash: " + Name;
-              // problem.AddVariables(_k);
-              //Console.WriteLine(Mixed.Temperature.WriteReport());
-
-
-              for (int i = 0; i < ComponentSet.Count; i++)
-              {
-                  System.EquationFactory.CreateKValueExpression(problem, System, _k[i], Mixed.Temperature, Mixed.Pressure, Liquid.GetMolarFractionArray(), Vapour.GetMolarFractionArray(), i);
-              }
-
-              var solver = new Newton();
-              problem.AddVariables(Mixed.Temperature);
-              if (_vf.Value <= 1e-10)
-              {
-                  problem.AddConstraints(Sym.Sum(0, NC, i => Mixed.ComponentMolarFraction[i] * _k[i]).IsEqualTo(1));
-              }
-              else if (_vf.Value >= 1 - 1e-10)
-              {
-                  problem.AddConstraints(Sym.Sum(0, NC, i => Mixed.Components[i].MolarFraction / _k[i]).IsEqualTo(1));
-              }
-              else
-              {
-                  var rachfordRice = Sym.Sum(0, NC, i => Mixed.Components[i].MolarFraction * Sym.Par(_k[i] - 1) / (_vf + Sym.Par(1 - _vf) * _k[i]));
-                  var rachfordRiceEq = rachfordRice.IsEqualTo(0);
-                  problem.AddConstraints(rachfordRiceEq);
-              }
-              solver.Solve(problem, 50, 0.95, 1e-6, true, true);
-          }
-
-          private void performEnthalpyUpdate(int NC)
-          {
-              Vapour.Enthalpy.Value = (Sym.Par(Sym.Sum(0, NC, (idx) => Vapour.Components[idx].MolarFraction * System.EquationFactory.GetVaporEnthalpyExpression(System, Mixed.Temperature, idx)))).Eval();
-              Liquid.Enthalpy.Value = (Sym.Par(Sym.Sum(0, NC, (idx) => Liquid.Components[idx].MolarFraction * System.EquationFactory.GetLiquidEnthalpyExpression(System, Mixed.Temperature, idx)))).Eval();
-
-              if (Double.IsNaN(Liquid.Enthalpy.Value))
-                  Liquid.Enthalpy.Value = 0;
-              if (Double.IsNaN(Vapour.Enthalpy.Value))
-                  Vapour.Enthalpy.Value = 0;
-
-              Mixed.Enthalpy.Value =
-                  ((Vapour.Molarflow * Vapour.Enthalpy + Liquid.Molarflow * Liquid.Enthalpy) / Mixed.Molarflow).Eval();
-          }
-
-          private void performDensityUpdate()
-          {
-              for (int i = 0; i < ComponentSet.Count; i++)
-              {
-                  Liquid.Components[i].MolarVolume.Value = 1.0 /
-                                                           System.EquationFactory.GetLiquidDensityExpression(System,
-                                                               Liquid.Temperature, i).Eval();
-                  Vapour.Components[i].MolarVolume.Value = 1.0 /
-                                                           System.EquationFactory.GetVapourDensityExpression(System,
-                                                               Liquid.Temperature, Vapour.Pressure, i).Eval();
-
-                  Mixed.Components[i].MolarVolume.Value =
-                      (Liquid.Components[i].MolarVolume + Vapour.Components[i].MolarVolume).Eval();
-              }
-          }
-
-
-          private void performMassBalance()
-          {
-              Vapour.Molarflow.Value = _vf.Value * Mixed.Molarflow.Value;
-
-              Liquid.Molarflow.Value = Mixed.Molarflow.Value - Vapour.Molarflow.Value;
-
-              for (int i = 0; i < ComponentSet.Count; i++)
-              {
-                  Liquid.Components[i].MolarFraction.Value = (Mixed.Components[i].MolarFraction / (1 + _vf * (_k[i] - 1))).Eval();
-                  Vapour.Components[i].MolarFraction.Value = (Liquid.Components[i].MolarFraction * _k[i]).Eval();
-
-                  Liquid.Components[i].MolarFlow.Value = Liquid.Components[i].MolarFraction.Value * Liquid.Molarflow.Value;
-                  Vapour.Components[i].MolarFlow.Value = Vapour.Components[i].MolarFraction.Value * Vapour.Molarflow.Value;
-
-                  Liquid.Components[i].MassFlow.Value =
-                      (Liquid.Components[i].MolarFlow *
-                       Sym.Convert(System.Components[i].PURE.MolarWeight,
-                           System.VariableFactory.Internal.MassUnit / System.VariableFactory.Internal.MoleUnit)).Eval();
-                  Vapour.Components[i].MassFlow.Value =
-                      (Vapour.Components[i].MolarFlow *
-                       Sym.Convert(System.Components[i].PURE.MolarWeight,
-                           System.VariableFactory.Internal.MassUnit / System.VariableFactory.Internal.MoleUnit)).Eval();
-                  Mixed.Components[i].MassFlow.Value =
-                      (Mixed.Components[i].MolarFlow *
-                       Sym.Convert(System.Components[i].PURE.MolarWeight,
-                           System.VariableFactory.Internal.MassUnit / System.VariableFactory.Internal.MoleUnit)).Eval();
-              }
-
-              Mixed.Massflow.Value = Mixed.GetMassFlowArray().Sum(v => v.Value);
-              Vapour.Massflow.Value = Vapour.GetMassFlowArray().Sum(v => v.Value);
-              Liquid.Massflow.Value = Liquid.GetMassFlowArray().Sum(v => v.Value);
-
-              for (int i = 0; i < ComponentSet.Count; i++)
-              {
-                  if (Liquid.Massflow.Value <= 1e-8)
-                      Liquid.Components[i].MassFraction.Value = 0;
-                  else
-                      Liquid.Components[i].MassFraction.Value = (Liquid.Components[i].MassFlow / Liquid.Massflow.Value).Eval();
-
-                  if (Vapour.Massflow.Value <= 1e-8)
-                      Vapour.Components[i].MassFraction.Value = 0;
-                  else
-                      Vapour.Components[i].MassFraction.Value = (Vapour.Components[i].MassFlow / Vapour.Massflow.Value).Eval();
-                  Mixed.Components[i].MassFraction.Value = (Mixed.Components[i].MassFlow / Mixed.Massflow.Value).Eval();
-              }
-          }*/
-        #endregion
+        
 
     }
 }
